48493c04b42f23b5324e1a6dc163d0467a2a383d
RP2510001\techcross <kukita.haruka@techcross.co.jp>
2025-10-17 11:51:59 +0900

    UIのコンポーネントアタッチ追加

--- UnityFigmaBridge/Editor/PrototypeFlow/PrototypeFlowManager.cs
+++ UnityFigmaBridge/Editor/PrototypeFlow/PrototypeFlowManager.cs
@@ -1,7 +1,10 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityFigmaBridge.Editor.Extension.ImportCache;
 using UnityFigmaBridge.Editor.FigmaApi;
 using UnityFigmaBridge.Editor.Nodes.DataMarker;
+using UnityFigmaBridge.Editor.Utils;
 using UnityFigmaBridge.Runtime.UI;
 using Color = UnityEngine.Color;
 
@@ -19,6 +22,7 @@ namespace UnityFigmaBridge.Editor.PrototypeFlow
             FigmaImportProcessData figmaImportProcessData)
         {
 
+            // デフォルトボタン
             if (CheckAddButtonBehaviour(node, figmaImportProcessData))
             {
                 if (nodeGameObject.GetComponent<Button>() == null)
@@ -84,6 +88,9 @@ namespace UnityFigmaBridge.Editor.PrototypeFlow
                 }
 
                 #endregion // ボタン
+                
+                #region トグル系 (チェックボックス・ラジオボタン・タブなど)
+                
                 // トグル
                 if (name.StartsWith("Toggle"))
                 {
@@ -94,6 +101,246 @@ namespace UnityFigmaBridge.Editor.PrototypeFlow
                     toggleMarker.commandKey = commandKey;
                     break;
                 }
+                
+                // チェックボックス
+                if (name.StartsWith("CheckboxGroup"))
+                {
+                    var checkBoxFlagManager = UnityUiUtils.GetOrAddComponent<CheckBoxFlagManager>(nodeGameObject);
+                    checkBoxFlagManager.AutoAssignee();
+                    break;
+                }
+
+                // ラジオボタン
+                if (name.StartsWith("RadioGroup"))
+                {
+                    var radioButtonManager = UnityUiUtils.GetOrAddComponent<RadioButtonManager>(nodeGameObject);
+                    radioButtonManager.AutoAssignee();
+                    break;
+                }
+                
+                // タブ
+                if (name.StartsWith("TabGroup"))
+                {
+                    var toggleGroup = UnityUiUtils.GetOrAddComponent<ToggleGroup>(nodeGameObject);
+                    toggleGroup.allowSwitchOff = false;
+                    var children = nodeGameObject.GetComponentsInChildren<Transform>(true);
+                    foreach (var child in children)
+                    {
+                        var childName = child.name;
+                        if (childName.StartsWith("Tab"))
+                        {
+                            var toggle = UnityUiUtils.GetOrAddComponent<Toggle>(child);
+                            toggle.group = toggleGroup;
+                            toggle.graphic = child.GetComponentInChildren<Image>();
+                        }
+                    }
+
+                    break;
+                }
+                
+                #endregion // トグル系 (チェックボックス・ラジオボタン・タブなど)
+
+                #region ドロップダウン
+
+                if (name.StartsWith("Dropdown"))
+                {
+                    var dropdown = UnityUiUtils.GetOrAddComponent<TMP_Dropdown>(nodeGameObject);
+                    UnityUiUtils.GetOrAddComponent<DropDownArrowController>(nodeGameObject);
+                    // ドロップダウンの要素初期化
+                    dropdown.options.Clear();
+                    dropdown.targetGraphic = nodeGameObject.GetComponent<Image>();
+                    var basePos = dropdown.transform.position;
+
+                    foreach (RectTransform child in nodeGameObject.transform)
+                    {
+                        switch (child.name)
+                        {
+                            case "Label":
+                                dropdown.captionText = child.GetComponent<TMP_Text>();
+                                break;
+                            case "PanelList":
+                                dropdown.template = child;
+                                var scrollRect = UnityUiUtils.GetOrAddComponent<ScrollRect>(child);
+                                var baseToList = child.position - basePos;
+                                var sizeUnitY = child.rect.height * child.pivot.y;
+                                // 下方向展開の場合
+                                if (baseToList.y < 0)
+                                {
+                                    child.pivot = new Vector2(child.pivot.x, 1.0f);
+                                    child.localPosition += new Vector3(0, sizeUnitY, 0);
+                                }
+                                // 上方向展開の場合
+                                else
+                                {
+                                    child.pivot = new Vector2(child.pivot.x, 0.0f);
+                                    child.localPosition += new Vector3(0, -sizeUnitY, 0);
+                                }
+
+                                var grandChildren = child.GetComponentsInChildren<RectTransform>(true);
+                                bool isFirstItem = true;
+                                foreach (var grandChild in grandChildren)
+                                {
+                                    if (!grandChild) continue;
+
+                                    switch (grandChild.name)
+                                    {
+                                        // 展開時の背面画像
+                                        case "ContentBackGround":
+                                            grandChild.ApplyAnchorPreset(UnityUiUtils.AnchorPreset.StretchFull);
+                                            break;
+                                        // 展開部分表示領域
+                                        case "ViewPort":
+                                            UnityUiUtils.GetOrAddComponent<RectMask2D>(grandChild);
+                                            scrollRect.viewport = grandChild;
+                                            break;
+                                        // コンテンツ部分
+                                        case "Content":
+                                            scrollRect.content = grandChild;
+                                            break;
+                                        // スクロールバー(縦)
+                                        case "ScrollbarVertical":
+                                            var scrollbar = UnityUiUtils.GetOrAddComponent<Scrollbar>(grandChild);
+                                            scrollRect.verticalScrollbar = scrollbar;
+
+                                            foreach (RectTransform scrollbarChild in grandChild)
+                                            {
+                                                if (scrollbarChild.name.Equals("Handle"))
+                                                {
+                                                    scrollbar.targetGraphic = scrollbarChild.GetComponent<Image>();
+                                                    scrollbar.handleRect = scrollbarChild;
+                                                    break;
+                                                }
+                                            }
+
+                                            break;
+                                        default:
+                                            if (!grandChild.name.StartsWith("Item"))
+                                            {
+                                                break;
+                                            }
+
+                                            // アイテム
+
+                                            // 項目名を収集
+                                            var itemLabel = grandChild.GetComponentInChildren<TMP_Text>(true);
+                                            if (itemLabel != null)
+                                            {
+                                                var optionData = new TMP_Dropdown.OptionData(itemLabel.text);
+                                                dropdown.options.Add(optionData);
+                                            }
+
+                                            // 最初のアイテムだった場合、テンプレ用にトグルを設定
+                                            if (isFirstItem)
+                                            {
+                                                isFirstItem = false;
+                                                var toggle = UnityUiUtils.GetOrAddComponent<Toggle>(grandChild);
+
+                                                foreach (RectTransform itemChild in grandChild)
+                                                {
+                                                    switch (itemChild.name)
+                                                    {
+                                                        // チェックマーク
+                                                        case "ItemCheckmark":
+                                                            itemChild.gameObject.SetActive(true);
+                                                            var checkMarkImage = itemChild.GetComponent<Image>();
+                                                            toggle.graphic = checkMarkImage;
+                                                            break;
+                                                        // ホバー画像
+                                                        case "ItemBackGround":
+                                                            itemChild.gameObject.SetActive(true);
+                                                            var hoverImage = itemChild.GetComponent<Image>();
+                                                            if (hoverImage)
+                                                            {
+                                                                toggle.targetGraphic = itemChild.GetComponent<Image>();
+                                                                toggle.transition = Selectable.Transition.ColorTint;
+                                                                var colorBlock = toggle.colors;
+                                                                // ホバー画像なので、通常時透明、選択時白にする
+                                                                colorBlock.normalColor = Color.clear;
+                                                                colorBlock.highlightedColor = Color.clear;
+                                                                colorBlock.pressedColor = Color.white;
+                                                                colorBlock.selectedColor = Color.white;
+                                                                colorBlock.disabledColor = Color.clear;
+                                                                toggle.colors = colorBlock;
+                                                            }
+
+                                                            break;
+                                                    }
+                                                }
+
+                                                break;
+                                            }
+
+                                            // 最初以外のアイテムはテンプレートにはいらないので、削除
+                                            Object.DestroyImmediate(grandChild.gameObject);
+                                            break;
+                                    }
+                                }
+
+                                break;
+                        }
+                    }
+
+                    break;
+                }
+
+                #endregion // ドロップダウン
+                
+                #region 入力フィールド
+
+                bool isTextAreaOne = name.StartsWith("TextAreaOne");
+                if (isTextAreaOne || name.StartsWith("TextAreaMulti"))
+                {
+                    var inputField = UnityUiUtils.GetOrAddComponent<TMP_InputField>(nodeGameObject);
+                    UnityUiUtils.GetOrAddComponent<RectMask2D>(nodeGameObject);
+                    
+                    foreach (Transform child in nodeGameObject.transform)
+                    {
+                        switch (child.name)
+                        {
+                            // テキストエリア
+                            case "TextArea":
+                                inputField.textViewport = child as RectTransform;
+                                foreach (Transform grandChild in child)
+                                {
+                                    var text = grandChild.GetComponent<TMP_Text>();
+                                    // 初期テキスト
+                                    if(grandChild.name.Equals("SampleText"))
+                                    {
+                                        inputField.placeholder = text;
+                                        continue;
+                                    }
+                                    
+                                    // 入力テキスト
+                                    if(grandChild.name.Equals("InputText"))
+                                    {
+                                        inputField.textComponent = text;
+                                        continue;
+                                    }
+                                }
+                                break;
+                            // 背景
+                            case "Base":
+                                inputField.targetGraphic = child.GetComponent<Image>();
+                                break;
+                        }
+                        
+                        // 一行テキストの場合
+                        if (isTextAreaOne)
+                        {
+                            inputField.lineType = TMP_InputField.LineType.SingleLine;
+                        }
+                        // 複数行テキストの場合
+                        else
+                        {
+                            inputField.lineType = TMP_InputField.LineType.MultiLineNewline;
+                        }
+
+                        break;
+                    }
+                }
+
+                #endregion // 入力フィールド
+
             } while (false);
             // TODO:ここにコンポーネントアタッチの仕組みを記載する
 
