46d73aa69870d997208f72130583ef505471a10d
RP2510001\techcross <kukita.haruka@techcross.co.jp>
2025-10-17 11:47:52 +0900

    ラジオボタン・チェックボックスの管理クラス追加

---
+++ UnityFigmaBridge/Runtime/UI/CheckBoxFlagManager.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityFigmaBridge.Runtime.UI
+{
+    /// <summary>
+    /// チェックボックス群のフラグ管理を行うクラス
+    /// 最大32個のチェックボックスをまとまりで扱える
+    /// </summary>
+    public class CheckBoxFlagManager : MonoBehaviour
+    {
+        [SerializeField]private List<Toggle> toggles = new List<Toggle>();
+
+        [SerializeField, ReadOnly(true)]private int flags;
+        
+        public bool this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= 32)
+                {
+                    return false;
+                }
+                return (flags & (1 << index)) != 0;
+            }
+        }
+
+        private void Start()
+        {
+            var count = toggles.Count;
+            // 対応範囲は 32bit のため成形
+            if (count > 32)
+            {
+                Debug.LogError("Too many checkboxes");
+                count = 32;
+            }
+            
+            for(var i = 0; i < count; i++)
+            {
+                var toggle = toggles[i];
+                if(toggle == null)return;
+                var index = i;
+                toggle.onValueChanged.AddListener(isOn =>
+                {
+                    if (isOn)
+                    {
+                        flags |= 1 << index;
+                    }
+                    else
+                    {
+                        flags &= ~(1 << index);
+                    }
+                });
+            }
+        }
+
+        public void Add(Toggle toggle)
+        {
+            toggles.Add(toggle);
+        }
+
+        public void AllStateChange(bool isOn)
+        {
+            foreach (var toggle in toggles)
+            {
+                toggle.isOn = isOn;
+            }
+        }
+        
+        public void AllStateChange(int toggleStates)
+        {
+            var count = toggles.Count;
+            // 対応範囲は 32bit のため成形
+            if (count > 32)
+            {
+                Debug.LogError("Too many checkboxes");
+                count = 32;
+            }
+            for (var i = 0; i < count; i++)
+            {
+                toggles[i].isOn = ((1 << i) & toggleStates) != 0;
+            }
+        }
+
+        public void AutoAssignee()
+        {
+            toggles.Clear();
+            foreach (Transform child in transform)
+            {
+                if (!child.name.StartsWith("List")) continue;
+                
+                var toggle = child.GetComponent<Toggle>();
+                if (toggle == null)
+                {
+                    toggle = child.gameObject.AddComponent<Toggle>();
+                    if (toggle == null) continue;
+                }
+
+                toggles.Add(toggle);
+                var contentChildren = child.GetComponentsInChildren<Transform>(true);
+                foreach (var contentChild in contentChildren)
+                {
+                    if (contentChild.name.Contains("Check"))
+                    {
+                        toggle.graphic = contentChild.GetComponent<Image>();
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ UnityFigmaBridge/Runtime/UI/RadioButtonManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityFigmaBridge.Runtime.UI
+{
+    /// <summary>
+    /// ラジオボタン管理用クラス
+    /// （ラジオボタンの基本的な動作はToggleの設定で行う前提）
+    /// </summary>
+    public class RadioButtonManager : MonoBehaviour
+    {
+        [SerializeField]private List<Toggle> toggles = new List<Toggle>();
+        private int selectIndex = -1;
+        public int SelectIndex => selectIndex;
+        
+        private void Start()
+        {
+            for(var i = 0; i < toggles.Count; i++)
+            {
+                var toggle = toggles[i];
+                if(toggle == null)return;
+                var index = i;
+                toggle.onValueChanged.AddListener(isOn =>
+                {
+                    if (isOn)
+                    {
+                        selectIndex = index;
+                    }
+                });
+            }
+        }
+
+        public void Add(Toggle toggle)
+        {
+            toggles.Add(toggle);
+        }
+        
+        public void SetSelectedIndex(int index)
+        {
+            if(index < 0 || index >= toggles.Count)return;
+            toggles[index].isOn = true;
+        }
+
+        public void AutoAssignee()
+        {
+            toggles.Clear();
+            var toggleGroup = GetComponent<ToggleGroup>();
+            if (toggleGroup == null)
+            {
+                toggleGroup =  gameObject.AddComponent<ToggleGroup>();
+            }
+            toggleGroup.allowSwitchOff = false;
+            foreach (Transform child in transform)
+            {
+                if (!child.name.StartsWith("List")) continue;
+                
+                var toggle = child.GetComponent<Toggle>();
+                if (toggle == null)
+                {
+                    toggle = child.gameObject.AddComponent<Toggle>();
+                    if (toggle == null) continue;
+                }
+                
+                toggle.group = toggleGroup;
+                toggles.Add(toggle);
+                var contentChildren = child.GetComponentsInChildren<Transform>(true);
+                foreach (var contentChild in contentChildren)
+                {
+                    if (contentChild.name.Contains("Check"))
+                    {
+                        toggle.graphic = child.GetComponent<Image>();
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
