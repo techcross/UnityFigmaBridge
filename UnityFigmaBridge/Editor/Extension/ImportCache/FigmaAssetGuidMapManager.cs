03a09560969dee60d7789c307f5a6e9e4d6813f7
moriShigeyoshi2 <mori.shigeyoshi@techcross.co.jp>
2026-03-13 20:08:58 +0900

    /Custom/Cache/のパス変更

--- UnityFigmaBridge/Editor/Extension/ImportCache/FigmaAssetGuidMapManager.cs
+++ UnityFigmaBridge/Editor/Extension/ImportCache/FigmaAssetGuidMapManager.cs
@@ -7,7 +7,7 @@ namespace UnityFigmaBridge.Editor.Extension.ImportCache
 {
     public static class FigmaAssetGuidMapManager
     {
-        private static string ComponentCacheDataDir = "Assets/Figma/Custom/Cache/";
+        private static string ComponentCacheDataDir = "/Custom/Cache/";
         public enum AssetType
         {
             Component,
@@ -36,10 +36,11 @@ namespace UnityFigmaBridge.Editor.Extension.ImportCache
             map = ScriptableObject.CreateInstance<FigmaAssetGuidMapData>();
             _mapContenar.Add(assetType, map);
             
+            var cashDir = FigmaPaths.FigmaFileRootFolder + ComponentCacheDataDir;
             // ディレクトリなければ生成
-            if (!Directory.Exists(ComponentCacheDataDir))
+            if (!Directory.Exists(cashDir))
             {
-                Directory.CreateDirectory(ComponentCacheDataDir);
+                Directory.CreateDirectory(cashDir);
             }
             
             AssetDatabase.CreateAsset(map, path);
